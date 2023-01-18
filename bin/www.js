61ff45e10ff1f1d3cf211c3f78a950cd26d9c0b2
whlong1 <whunterlong@gmail.com>
2023-01-18 09:23:32 -0500

    Update starter

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@ const { app } = require('../server.js')
 const debug = require('debug')
 const http = require('http')
 
-const port = normalizePort(process.env.PORT || '3000')
+const port = normalizePort(process.env.PORT || '3001')
 app.set('port', port)
 
 const server = http.createServer(app)
